6ae666f2d69b72822c14003f90c46d899ced545b
wang <wang@192.168.0.2>
2025-03-12 16:58:12 +0800

    feat: 监控

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,7 +2,7 @@ import '@/app/ui/global.css';
 import { inter } from '@/app/ui/fonts';
 
 import { Metadata } from 'next';
- 
+import { Analytics } from "@vercel/analytics/react"
 export const metadata: Metadata = {
   title: {
     template: '%s',
